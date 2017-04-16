9d61f2ea7dc645b0f22dfb39e0ec6d6c81911c99
Jaber Molla <molla.jaber@gmail.com>
2017-04-17 00:37:00 +0600

    Plugin info updated

--- wp-ss-box.php
+++ wp-ss-box.php
@@ -1,12 +1,12 @@
 <?php 
 /* 
 Plugin Name: Switch CTA Box
-Plugin URI: http://wordpress.org/plugins/switch-cta-box
+Plugin URI: http://wppluginwiki.com/cta-box
 Description: Switch CTA BOX anywhere you want in wordpress(ex. wordpress pages, posts, widgets)
-Version: a1.0
-Author: @ash.if
+Version: 1.0 
+Author: Wp Plugin Wiki 
 Author URI: http://wppluginwiki.com/
-License: GPL2
+License: GPL2 
 */
 
 if ( ! defined( 'ABSPATH' ) ){
@@ -19,8 +19,18 @@ add_filter('widget_text','do_shortcode');
 function wppw_adding_cta_box_stylesheet() {
     wp_register_style(
         'newstyle',
-        plugins_url( '/inc/cta_style.css', __FILE__ )
+        plugins_url( '/css/cta_style.css', __FILE__ )
     );
     wp_enqueue_style( 'newstyle' );
 }
 add_action( 'wp_enqueue_scripts', 'wppw_adding_cta_box_stylesheet' );
+add_filter( 'gettext', 'change_publish_button', 10, 2 );
+
+function change_publish_button( $translation, $text ) {
+if ( 'ctabox' == get_post_type())
+if ( $text == 'Publish' )
+    return 'Save';
+else if ( $text == 'Save Draft' )
+	return 'Save CTA Box';
+return $translation;
+}
