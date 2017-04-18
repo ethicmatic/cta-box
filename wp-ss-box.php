346482f4ca3dc5ef8df214847b2ce96760d3eb0a
Jaber Molla <molla.jaber@gmail.com>
2017-04-18 16:47:57 +0600

    Updated to escape conflict.

--- wp-ss-box.php
+++ wp-ss-box.php
@@ -16,17 +16,17 @@ require_once ('inc/post_type.php');
 
 add_filter('widget_text','do_shortcode');
 
-function wppw_adding_cta_box_stylesheet() {
+function wppw_cttabox_adding_cta_box_stylesheet() {
     wp_register_style(
         'newstyle',
         plugins_url( '/css/cta_style.css', __FILE__ )
     );
     wp_enqueue_style( 'newstyle' );
 }
-add_action( 'wp_enqueue_scripts', 'wppw_adding_cta_box_stylesheet' );
-add_filter( 'gettext', 'change_publish_button', 10, 2 );
+add_action( 'wp_enqueue_scripts', 'wppw_cttabox_adding_cta_box_stylesheet' );
+add_filter( 'gettext', 'wppw_cttabox_change_publish_button', 10, 2 );
 
-function change_publish_button( $translation, $text ) {
+function wppw_cttabox_change_publish_button( $translation, $text ) {
 if ( 'ctabox' == get_post_type())
 if ( $text == 'Publish' )
     return 'Save';
